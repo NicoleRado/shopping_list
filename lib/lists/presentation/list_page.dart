19144ff66b251735bdc796836d581d9f9b849bde
Nicole Rado <nicole.rado@cgi.com>
2023-08-16 10:45:10 +0200

    feature(listPage): add defaultListPageView and loadedListPageView to listPage and add error Snackbar

--- lib/lists/presentation/list_page.dart
+++ lib/lists/presentation/list_page.dart
@@ -1,25 +1,60 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shopping_list/auth/application/auth_controller.dart';
+
+import '../application/list_controller.dart';
+import 'list_views/default_list_page_view.dart';
+import 'list_views/loaded_list_page_view.dart';
 
 class ListPage extends ConsumerWidget {
   const ListPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('ListPage'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).primaryColor,
-        actions: [
-          IconButton(
-              onPressed: () =>
-                  ref.read(authControllerProvider.notifier).signOut(),
-              icon: const Icon(Icons.logout))
-        ],
+    final String title = tr('list_page.list_page_title');
+
+    ref.listen(
+      listControllerProvider.select(
+        (currentState) => currentState.maybeWhen(
+          isFailure: (failure) => failure,
+          orElse: () => null,
+        ),
       ),
-      body: const Text('LogedIn'),
+      ((previous, next) {
+        if (next != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                next.when(
+                  createListFailure: () =>
+                      tr('list_page.list_error.create_list_failure'),
+                  joinListFailure: () =>
+                      tr("list_page.list_error.join_list_failure"),
+                  deleteListFailure: () =>
+                      tr("list_page.list_error.delete_list_failure"),
+                  exitListFailure: () =>
+                      tr("list_page.list_error.exit_list_failure"),
+                ),
+              ),
+            ),
+          );
+        }
+      }),
     );
+
+    return ref.watch(listControllerProvider).maybeWhen(
+          isLoading: () => DefaultListPageView(
+            title: title,
+            child: const CircularProgressIndicator(),
+          ),
+          isSuccess: (user) => LoadedListPageView(
+            user: user,
+            title: title,
+          ),
+          orElse: () => DefaultListPageView(
+            title: title,
+            child: Container(),
+          ),
+        );
   }
 }
